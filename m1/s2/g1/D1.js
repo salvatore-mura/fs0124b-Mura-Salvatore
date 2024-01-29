eba9b32b7eb0a688e7411b2b0229f849853a01a8
SalvatoreMura <tore.291196@gmail.com>
2024-01-29 14:19:47 +0100

    Javascript g1

---
+++ m1/s2/g1/D1.js
@@ -0,0 +1,93 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser.
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA
+STRINGA = Il dato tipo stringa è semplicemente del testo, ed è necessario scriverlo dentro le '' o ""
+NUMERO = Il dato tipo numerico può assumere i caratteri numerici da 1 a 9; è consentito con esso fare delle operaizoni matematiche
+BOOLEAN = Acceso o Spento, Vero o Falso, 0 o 1... Usato per validare la presenza o meno di qualcosa, o la sua a accertazione (risponde a domande che hanno come risposta o si o no)
+ARRAY = L'array è un insieme di vsariabili, dichiarate nell'array stesso o utilizzando nomi di variabili già esistenti prima. Vieni definito con le parentesi quadre.
+OGGETTO = E' un insieme di coppie chiave valore. Viene definito con le parentesi graffe.
+UNDEFINED = E' il valore base che ha una variabile quando non li viene assegnato un valore. Quindi sta a significsare che il valore non è definito, ma non che è 'niente'.
+NULL = Sta a significare che il valore è nullo, da non confondere con undefined che è un valore che non è definito
+*/
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let myName = 'Salvatore'
+// console.log(myName)
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+// let a = 12
+// let b = 20
+// let sum = a + 20
+// console.log (sum)
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA 
+let x
+x = 12
+console.log(x)*/
+
+
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */ 
+
+// const myName = 'Salvatore'
+// myName = 'Mura'
+// console.log(myName)
+
+
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let x = 12
+// let sub = 4 - x
+// console.log (sub)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// let name1 = "jhon"
+// let name2 = "Jhon"
+
+// console.log (name1 === name2)
+
+// name1.toLocaleLowerCase()
+
+// console.log (name2 === name2)
